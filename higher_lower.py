141f14ab6d7712239ee0fee8faf63546de15cfe6
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-01-13 22:19:43 +0100

    Function: easy mode

--- higher_lower.py
+++ higher_lower.py
@@ -6,3 +6,14 @@ game_on = None
 answers = None
 magic_number = None
 
+def easy_mode():
+    global magic_number
+    magic_number = int(random.randrange(0,100))
+    while game_on:
+        user_guess = int(input('Guess the number: '))
+        if user_guess > magic_number:
+            print('Too high. Try again.')
+        elif user_guess < magic_number:
+            print('Too low. Try again.')
+        else:
+            print('Great! You won!')
