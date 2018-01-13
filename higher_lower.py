2d2dd30e209a4c5ff3b99664249c48d203c98485
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-01-13 22:37:38 +0100

    Function easy_mode improvement

--- higher_lower.py
+++ higher_lower.py
@@ -8,12 +8,19 @@ magic_number = None
 
 def easy_mode():
     global magic_number
-    magic_number = int(random.randrange(0,100))
-    while game_on:
-        user_guess = int(input('Guess the number: '))
+    magic_number = int(random.randrange(0,11))
+    global game_on
+    game_on = True
+    while game_on is True:
+        user_guess = int(input('Guess the number from range 0-10: '))
         if user_guess > magic_number:
             print('Too high. Try again.')
         elif user_guess < magic_number:
             print('Too low. Try again.')
         else:
             print('Great! You won!')
+            again = input('Do you want play again? Type YES or NO: ')
+            if again == 'YES':
+                easy_mode()
+            else:
+                quit()
